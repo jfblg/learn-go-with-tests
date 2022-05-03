5a710a34b2221c85a1ffcc49c88c56287391978b
jfblg <inbox.jfblg@gmail.com>
2022-05-03 20:10:08 +0200

    add arrays package

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,8 +1,9 @@
 package arrays
 
 import "testing"
+import "reflect"
 
-func TestGOSum(t *testing.T) {
+func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
@@ -12,6 +13,37 @@ func TestGOSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d, want %d, given %v", got, want, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{0, 9}, []int{1, 2})
+	want := []int{9, 3}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+
+	t.Run("Sum some slice", func(t *testing.T) {
+		got := SumAllTails([]int{0, 1, 2}, []int{1, 2})
+		want := []int{3, 2}
+		checkSums(t, got, want)
+
+	})
+
+	t.Run("Run empty slice safely", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{1, 2})
+		want := []int{0, 2}
+		checkSums(t, got, want)
 	})
 
 }
