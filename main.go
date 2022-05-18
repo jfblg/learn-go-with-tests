c0ac91c7fcdc25e32c33416bbdef93f3379edc05
jfblg <inbox.jfblg@gmail.com>
2022-05-18 05:33:39 +0200

    mocking - remove DefaultSleeper

--- main.go
+++ main.go
@@ -6,12 +6,6 @@ import (
 	"time"
 )
 
-type DefaultSleeper struct{}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
-
 func main() {
 	// sleeper := &DefaultSleeper{}
 	sleeper := &mocking.ConfigurableSleeper{1 * time.Second, time.Sleep}
