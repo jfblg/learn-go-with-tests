db09f6c00d1465c728ad82bab1a714c5583a855b
jfblg <inbox.jfblg@gmail.com>
2022-07-07 08:12:25 +0200

    add code to arrays_generics

--- arrays_generics/sum.go
+++ arrays_generics/sum.go
@@ -1,5 +1,6 @@
 package arraysgenerics
 
+// Sum calculates the total from a slice of numbers
 func Sum(numbers []int) int {
 	sum := 0
 
@@ -9,6 +10,7 @@ func Sum(numbers []int) int {
 	return sum
 }
 
+//
 func SumAll(numbersToSum ...[]int) []int {
 	var sums []int
 	for _, numbers := range numbersToSum {
@@ -18,6 +20,7 @@ func SumAll(numbersToSum ...[]int) []int {
 	return sums
 }
 
+// SumAllTails calculates the sums of all but the first number given a collection of slices
 func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 	for _, numbers := range numbersToSum {
@@ -31,4 +34,32 @@ func SumAllTails(numbersToSum ...[]int) []int {
 	}
 
 	return sums
+}
+
+func Reduce[A any](collection []A, accumulator func(A, A) A, initialValue A) A {
+	var result = initialValue
+	for _, x := range collection {
+		result = accumulator(result, x)
+	}
+	return result
+}
+
+// SumGeneric calculates the total from a slice of numbers
+func SumGenerics(numbers []int) int {
+	add := func(acc, x int) int { return acc + x }
+	return Reduce(numbers, add, 0)
+}
+
+// SumAllTailGenerics calculates the sums of all but the fiest number of a given collection of slices
+func SumAllTailGenerics(numbers ...[]int) []int {
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
+		} else {
+			tail := x[1:]
+			return append(acc, SumGenerics(tail))
+		}
+	}
+
+	return Reduce(numbers, sumTail, []int{})
 }
--- arrays_generics/sum_test.go
+++ arrays_generics/sum_test.go
@@ -1,13 +1,25 @@
 package arraysgenerics
 
-import "testing"
-import "reflect"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
 		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d, want %d, given %v", got, want, numbers)
+		}
+	})
+	t.Run("SumGenerics - collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := SumGenerics(numbers)
 		want := 6
 
 		if got != want {
@@ -25,25 +37,69 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
-
-	checkSums := func(t testing.TB, got, want []int) {
-		t.Helper()
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v, want %v", got, want)
+	t.Run("TestSumAllTails", func(t *testing.T) {
+		checkSums := func(t testing.TB, got, want []int) {
+			t.Helper()
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("got %v, want %v", got, want)
+			}
 		}
+
+		t.Run("Sum some slice", func(t *testing.T) {
+			got := SumAllTails([]int{0, 1, 2}, []int{1, 2})
+			want := []int{3, 2}
+			checkSums(t, got, want)
+
+		})
+
+		t.Run("Run empty slice safely", func(t *testing.T) {
+			got := SumAllTails([]int{}, []int{1, 2})
+			want := []int{0, 2}
+			checkSums(t, got, want)
+		})
+	})
+	t.Run("TestSumAllTails generics", func(t *testing.T) {
+		checkSums := func(t testing.TB, got, want []int) {
+			t.Helper()
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("got %v, want %v", got, want)
+			}
+		}
+
+		t.Run("Sum some slice", func(t *testing.T) {
+			got := SumAllTailGenerics([]int{0, 1, 2}, []int{1, 2})
+			want := []int{3, 2}
+			checkSums(t, got, want)
+
+		})
+
+		t.Run("Run empty slice safely", func(t *testing.T) {
+			got := SumAllTailGenerics([]int{}, []int{1, 2})
+			want := []int{0, 2}
+			checkSums(t, got, want)
+		})
+	})
+}
+
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+		AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(a, b string) string {
+			return a + b
+		}
+		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
+	})
+}
+
+func AssertEqual[C comparable](t *testing.T, got, want C) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
 	}
-
-	t.Run("Sum some slice", func(t *testing.T) {
-		got := SumAllTails([]int{0, 1, 2}, []int{1, 2})
-		want := []int{3, 2}
-		checkSums(t, got, want)
-
-	})
-
-	t.Run("Run empty slice safely", func(t *testing.T) {
-		got := SumAllTails([]int{}, []int{1, 2})
-		want := []int{0, 2}
-		checkSums(t, got, want)
-	})
-
 }
